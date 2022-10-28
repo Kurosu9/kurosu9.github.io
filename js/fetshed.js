c9668408d8f1ff21a6ca493c323133a2d1abe50e
Kurosu9 <kurstan.abazbekov@alatoo.edu.kg>
2022-10-28 12:03:52 +0600

    twentyone commit

--- js/fetshed.js
+++ js/fetshed.js
@@ -1,12 +1,12 @@
-// const loadData = () => {
-//     fetch(
-//         'https://api.catboys.com/img'
-//     ).then((resp) => {
-//         return resp.json();
-//     }).then((data) => {
-//         console.log(data.file);
-//         document.getElementById("anime").src = data.url;
-//     }).catch(() => {
-//         console.log("error");
-//     })
-// }
+const loadData = () => {
+    fetch(
+        'https://api.catboys.com/img'
+    ).then((resp) => {
+        return resp.json();
+    }).then((data) => {
+        console.log(data.file);
+        document.getElementById("anime").src = data.url;
+    }).catch(() => {
+        console.log("error");
+    })
+}
