04844b7b4cbb35184a3282e91ce2f39db326c268
Kurosu9 <kurstan.abazbekov@alatoo.edu.kg>
2022-10-29 23:12:01 +0600

    twenty ninth commit

--- js/fetshed.js
+++ js/fetshed.js
@@ -1,12 +1,12 @@
-const loadData = () => {
-    fetch(
-        'https://api.catboys.com/img'
-    ).then((resp) => {
-        return resp.json();
-    }).then((data) => {
-        console.log(data.file);
-        document.getElementById("anime").src = data.url;
-    }).catch(() => {
-        console.log("error");
-    })
+const ani = document.querySelector("#ani");
+const result = document.querySelector("#result");
+
+ani.addEventListener("click", getRandomData);
+
+function getRandomData() {
+    fetch("https://api.waifu.pics/sfw/happy")
+        .then(response => response.json())
+        .then(data => {
+            result.innerHTML = `<img src="${data.url}" width="300px" height="250px">`
+        })
 }
